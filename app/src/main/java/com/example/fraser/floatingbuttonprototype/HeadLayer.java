b0e86f4de2b0ffae8f82b27e6e99bb60167d021a
fraser0319 <fraser0319@gmail.com>
2017-02-18 19:12:53 +0000

    added what ive done so far, ive basicly got so it will start the floating button service with the start stop buttons in the toolbar

---
+++ app/src/main/java/com/example/fraser/floatingbuttonprototype/HeadLayer.java
@@ -0,0 +1,149 @@
+package com.example.fraser.floatingbuttonprototype;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+/**
+ * Created by Fraser on 01/12/2016.
+ */
+
+public class HeadLayer extends View {
+
+    private Context mContext;
+    private FrameLayout mFrameLayout;
+    private WindowManager mWindowManager;
+    private boolean shouldClick;
+    private boolean openClose = false;
+
+    public HeadLayer(Context context) {
+        super(context);
+
+        mContext = context;
+        mFrameLayout = new FrameLayout(mContext);
+
+        addToWindowManager();
+        //createOnTouchEvent();
+
+    }
+
+    private void addToWindowManager() {
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+//                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,// allows you to use the back button properly
+                PixelFormat.TRANSLUCENT);
+//        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+        params.gravity = Gravity.LEFT;
+
+        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        mWindowManager.addView(mFrameLayout, params);
+
+        LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // Here is the place where you can inject whatever layout you want.
+        layoutInflater.inflate(R.layout.head, mFrameLayout);
+        //final ImageView imageView = (ImageView) mFrameLayout.findViewById(R.id.imageView);
+        // add a click listener to the image view
+        final ImageView imageView = (ImageView) mFrameLayout.findViewById(R.id.imageView);
+//        imageView.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.e("here", "icon click");
+//                if(!openClose){
+//                    Intent intent = new Intent(getContext(), testActivity.class);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // clears activity to open the new small activity
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    getContext().startActivity(intent);
+//                    openClose = true;
+//                }else{
+//                    Intent intent = new Intent(getContext(), MainActivity.class);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // clears activity to open the main activity
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    getContext().startActivity(intent);
+//                    openClose = false;
+//                }
+//            }
+//        });
+
+
+
+        // Support dragging the image view
+        imageView.setOnTouchListener(new OnTouchListener() {
+            private static final int MAX_CLICK_DURATION = 200;
+            private long startClickTime;
+            private int initX, initY;
+            private int initTouchX, initTouchY;
+
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getRawX();
+                int y = (int) event.getRawY();
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        startClickTime = System.currentTimeMillis();
+                        Log.e("Down","touch");
+                        //shouldClick = false;
+                        initX = params.x;
+                        initY = params.y;
+                        initTouchX = x;
+                        initTouchY = y;
+
+                        break;
+                    }
+                    case MotionEvent.ACTION_MOVE: {
+
+                        params.x = initX + (x - initTouchX);
+                        params.y = initY + (y - initTouchY);
+                        mWindowManager.updateViewLayout(mFrameLayout, params);
+                        return true;
+
+                    }
+
+                    case MotionEvent.ACTION_UP: {
+                        long clickDuration = System.currentTimeMillis() - startClickTime;
+
+                        if(clickDuration < MAX_CLICK_DURATION) {
+                            //click event has occurred
+
+                            if(!openClose){
+                                Intent intent = new Intent(getContext(), testActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // clears activity to open the new small activity
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                getContext().startActivity(intent);
+                                openClose = true;
+                            }else{
+//                                Intent intent = new Intent(getContext(),MainActivity.class);
+//                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // clears activity to open the main activity
+//                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                                getContext().startActivity(intent);
+
+                                openClose = false;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+        });
+    }
+    /**
+     * Removes the view from window manager.
+     */
+    public void destroy() {
+        mWindowManager.removeView(mFrameLayout);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/fraser/floatingbuttonprototype/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.fraser.floatingbuttonprototype;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private ImageButton startNotification;
+    private ImageButton endNotification;
+    private static boolean serviceState;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private PermissionChecker mPermissionChecker;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
+       // setUpTabs();
+
+        if (getIntent().getExtras() != null) {
+            serviceState = getIntent().getExtras().getBoolean("state");
+        }
+        checkPermissions();
+        getServiceState();
+    }
+
+    public void checkPermissions(){
+        mPermissionChecker = new PermissionChecker(this);
+        if(!mPermissionChecker.isRequiredPermissionGranted()){
+            Intent intent = mPermissionChecker.createRequiredPermissionIntent();
+            startActivityForResult(intent, PermissionChecker.REQUIRED_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public boolean getServiceState() {
+        SharedPreferences sharedPrefs = getSharedPreferences("com.example.fraser.floatingbuttonprototype", MODE_PRIVATE);
+        Log.e("service_state", serviceState + "");
+        startNotification = (ImageButton) findViewById(R.id.start_notification);
+        endNotification = (ImageButton) findViewById(R.id.end_notification);
+        if (sharedPrefs.getBoolean("service_status", serviceState)) {
+            startNotification.setEnabled(false);
+            endNotification.setEnabled(true);
+            endNotification.setAlpha((float) 1);
+            startNotification.setAlpha((float) 0.2);
+            return true;
+        } else {
+            endNotification.setEnabled(false);
+            startNotification.setEnabled(true);
+            startNotification.setAlpha((float) 1);
+            endNotification.setAlpha((float) 0.2);
+            return false;
+        }
+    }
+
+    public void startService(View v){
+        serviceState = true;
+        SharedPreferences.Editor editor = getSharedPreferences("com.example.fraser.floatingbuttonprototype", MODE_PRIVATE).edit();
+        editor.putBoolean("service_status", serviceState);
+        editor.commit();
+
+        getServiceState();
+        Intent service = new Intent(getApplicationContext(), HeadService.class);
+        startService(service);
+    }
+
+    public void stopService(View v){
+        serviceState = false;
+        SharedPreferences.Editor editor = getSharedPreferences("com.example.fraser.floatingbuttonprototype", MODE_PRIVATE).edit();
+        editor.putBoolean("service_status", serviceState);
+        editor.commit();
+
+        getServiceState();
+        Intent intent = new Intent(getApplicationContext(), HeadService.class);
+        stopService(intent);
+        Toast.makeText(getApplicationContext(), "service stopped", Toast.LENGTH_LONG).show();
+    }
+
+
+    private void setUpTabs() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        //viewPager = (ViewPager) findViewById(R.id.viewpager);
+        //setUpPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        //tabLayout.setupWithViewPager(viewPager);
+    }
+
+    //persmission method.
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+}
